540f971499c1371a4a29266a9814823fffeb910a
Florian Godfrey Link <fgodfreylink@pugetsound.edu>
2021-10-03 23:05:43 -0700

    Added the main gameplay loop that parses guesses and updates guessedWords accordingly, and ends the game if all words have been guessed or the guess is null

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var minLength = 3;
 var maxLength = 6;
 var validRootWords = [];
 var unscrambledWords = [];
+var guessedWords = [];
 
 //Finds all words from the dictionary that are 6 letters long that can possibly be used as the base word
 function findValidRootWords() {
@@ -21,7 +22,8 @@ function getUnscrambledWords (perms){
     }
 }
 
-function shuffle(word){
+//randomly shuffles the root word
+function scramble(word){
     word = word.split("");
     var l = word.length();
     for(var i=0; i<word.length(); i++){
@@ -43,3 +45,48 @@ findValidRootWords();
 var rootWord = validRootWords[Math.floor(Math.random() * validRootWords.length())];
 var perms = getPermutations(rootWord);
 getUnscrambledWords(perms);
+rootWord = scramble(rootWord);
+
+//Begin main guessing loop
+do {
+    console.log("Available letters: " + rootWord + "\n");
+    for(var i=0; i<unscrambledWords.length(); i++){
+        if(!guessedWords.includes(unscrambledWords[i]))
+            for(int j=0; j<unscrambledWords[i].length(); j++){
+                console.log("- ");
+            }
+            console.log("\n");
+        }
+    }
+    for(var k=0; k<guessedWords.length(); k++){
+        console.log(guessedWords[k] + "\n");
+    }
+    guess = prompt("Enter a guess: ");
+    if(guess != null){
+        if(guess.length()<minLength){
+            if(guess == "*"){
+                rootWord = scramble(rootWord);
+                alert("Shuffling root word...");
+            }
+            alert("Guess is too short!");
+        }
+        else if(guess.length() > maxLength){
+            alert("Guess is too long!");
+        }
+        else if(guessedWords.includes(guess)){
+            alert("Already guessed " + guess + "!");
+        }
+        else if(unscrambledWords.includes(guess)){
+            alert("Correct! " + guess);
+            guessedWords.push(guess);
+        }
+        else{
+            alert(guess + " is not a word!");
+        }
+    }
+    console.clear();
+} while (guess != null && guessedWords.length() < unscrambledWords.length());
+console.log("You guessed " + guessedWords.length() + " out of" + unscrambledWords.length();)
+for(var l=0; l<unscrambledWords.length(); l++){
+    console.log(unscrambledWords[l] + "\n");
+}
