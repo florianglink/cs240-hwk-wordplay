fa66130ab314347251f2d2fce3d8cb0af0be75a6
Florian Godfrey Link <fgodfreylink@pugetsound.edu>
2021-10-05 17:29:36 -0700

    actual final commit. Added block and inline comments

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ var unscrambledWords = [];
 var guessedWords = [];
 var validWordsInRange = [];
 
-//Removes words from dictionary that are longer than max length or shorter than min length
+/*
+Removes words from dictionary that are longer than max length or shorter than min length
+@param dictionary the list of words that you want to trim
+*/
 function trimDictionary(dictionary){
     for(var i=0; i<dictionary.length; i++){
         let word = dictionary[i];
@@ -15,7 +18,9 @@ function trimDictionary(dictionary){
     }
 }
 
-//Finds all words from the dictionary that are 6 letters long that can possibly be used as the base word
+/*
+Finds all words from the dictionary that are 6 letters long that can possibly be used as the base word
+*/
 function findValidRootWords() {
     for (var i = 0; i<dictionary.length; i++){
         if(dictionary[i].length == maxLength){
@@ -24,7 +29,10 @@ function findValidRootWords() {
     }
 }
 
-//randomly shuffles the root word
+/*
+Randomly shuffles the root word
+@param word the word that is ti be shuffled
+*/
 function scramble(word){
     var l = word.length;
     for(var i=0; i<word.length; i++){
@@ -38,8 +46,12 @@ function scramble(word){
     return word;
 }
 
-//Determines if a given word can be created with a given set of letters.
-//Accounts for the fact that a letter can only be used more than once if it occurs in the given letters more than once.
+/*
+Determines if a given word can be created with a given set of letters.
+Accounts for the fact that a letter can only be used more than once if it occurs in the given letters more than once.
+@param testWord the given word to be checked
+@param givenLetters the available letters that can be used to make testWord
+*/
 function isWordFromLetters(testWord, givenLetters){
     var testLetters = testWord.split("");
     var validLetters = givenLetters.split("");
@@ -58,7 +70,10 @@ function isWordFromLetters(testWord, givenLetters){
     }
 }
 
-//Traverses the dictionary to find all possible words that can be made using the letters in the root word
+/*
+Traverses the dictionary to find all possible words that can be made using the letters in the root word
+@param rootWord the word to be checked against the dictionary to see what other words can be made from its letters
+*/
 function findWordsToBeGuessed(rootWord){
     for(var i=0; i<validWordsInRange.length; i++){
         if(isWordFromLetters(validWordsInRange[i], rootWord)){
@@ -70,28 +85,27 @@ function findWordsToBeGuessed(rootWord){
 //Startup
 trimDictionary(dictionary);     //get rid of all words that are too short/too long
 findValidRootWords();           //get all 6 letter words that could be used as the root word
-var rootWord = validRootWords[Math.floor(Math.random() * validRootWords.length)]; 
-findWordsToBeGuessed(rootWord);
-console.log(unscrambledWords);
+var rootWord = validRootWords[Math.floor(Math.random() * validRootWords.length)]; //pick a root word
+findWordsToBeGuessed(rootWord);            //find all other valaid words that can be made with the letters of the root word
 rootWord = scramble(rootWord);
 
 //Begin main guessing loop
 do {
-    console.log("Available letters: " + rootWord + "\n");
+    console.log("Available letters: " + rootWord + "\n");    //print the available letters
     let blanks = "";
     for(var i=0; i<unscrambledWords.length; i++){
         if(!guessedWords.includes(unscrambledWords[i])){
             for(var j=0; j<unscrambledWords[i].length; j++){
-                blanks += "- ";
+                blanks += "- ";                            //print dashes in place of words that haven't been guessed yet
             }
             blanks += "\n";
         }
     }
     console.log(blanks);
-    for(var i=0; i<guessedWords.length; i++){
+    for(var i=0; i<guessedWords.length; i++){            //print all words that have been guessed
         console.log(guessedWords[i] + "\n");
     }
-    guess = prompt("Enter a guess: ");
+    guess = prompt("Enter a guess: ");    //parse the user guess and respond accordingly
     if(guess != null){
         if(guess.length<minLength){
             if(guess == "*"){
@@ -119,8 +133,9 @@ do {
     console.clear();
 } while (guess != null && guessedWords.length < unscrambledWords.length); 
 
-console.log("You guessed " + guessedWords.length + " out of " + unscrambledWords.length);
+
+console.log("You guessed " + guessedWords.length + " out of " + unscrambledWords.length + "!");  //game over, report result
 for(var l=0; l<unscrambledWords.length; l++){
-    console.log(unscrambledWords[l] + "\n");
+    console.log(unscrambledWords[l] + "\n");   //print all the words that may not have been guessed
 }
 
